143d6569289623823c9f08775db5e51b2ce95349
jin <jinchul112@gmai.com>
2021-07-30 18:09:08 +0200

    commit

---
+++ client.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom'
+
+import App from './layout/app'
+
+render(<App />, document.querySelector('#app'))
---
+++ layout/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<>
+			soon
+		</>
+	)
+}
+
+export default App
